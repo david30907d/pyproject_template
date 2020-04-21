ce7a758e7e910d809452da0f6694d7a42a9f537e
davidtnfsh <david.chang@lndata.com>
2020-04-21 10:23:19 +0800

    test(coverage): show coverage at every run

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,8 @@ import pytest
 from project.main import demo
 
 
-def test_demo():
+def test_demo() -> None:
+    # Secure one
     if __debug__:
         if demo() != 200:
             raise AssertionError("status code error!")
