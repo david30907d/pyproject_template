3a22eb9770994f7c266114384dcd30a3ba881153
davidtnfsh <davidtnfsh@gmail.com>
2020-08-06 16:54:05 +0800

    fix(security): upgrade dot-prod

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,13 @@
-import pytest
-
+"""
+test file
+"""
 from project.main import demo
 
 
 def test_demo() -> None:
+    """
+    test function of demo
+    """
     # Secure one
     if __debug__:
         if demo() != 200:
